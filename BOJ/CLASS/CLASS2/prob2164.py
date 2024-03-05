ccec79b85b43e61fd5d5f5da5aba3829cd72620f
yunjikwak <mia007478@gmail.com>
2024-03-05 17:59:45 +0900

    CLASS 2

--- BOJ/CLASS/CLASS2/prob2164.py
+++ BOJ/CLASS/CLASS2/prob2164.py
@@ -1,4 +1,18 @@
+from collections import deque
+
 N = int(input())
+
+queue = deque()
+for i in range(1, N+1):
+    queue.append(i)
+
+while len(queue) != 1:
+    queue.popleft()
+    if len(queue) == 1:
+        break
+    queue.append(queue.popleft())
+    
+print(queue.popleft())
 
 """
 1234
