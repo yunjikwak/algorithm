8c079b5e8f53425a10add1c757c65f78a15130b2
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-06-01 23:35:30 +0900

    Title: 개발팀의 능력, Time: 40ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250601/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -0,0 +1,30 @@
+numbers = list(map(int, input().split()))
+N = len(numbers)
+
+def get_diff(a, b):
+    sum1 = numbers[a] + numbers[b]
+    
+    min_diff = sum(numbers)
+    for i in range(N):
+        if i == a or i == b:
+            continue
+
+        sum2 = sum(numbers) - sum1 - numbers[i]
+        if sum1 == sum2 or sum1 == i or sum2 == numbers[i]:
+            continue
+
+        minVal = min(sum1, sum2, numbers[i])
+        maxVal = max(sum1, sum2, numbers[i])
+        min_diff = min(min_diff, maxVal-minVal)
+
+    return min_diff
+
+min_dif = sum(numbers)
+for i in range(N):
+    for j in range(i, N):
+        min_dif = min(min_dif, get_diff(i, j))
+if min_dif == sum(numbers):
+    print(-1)
+else:
+    print(min_dif)
+        
