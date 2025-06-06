bdce78582a9cb6cb0b80017e890812ef201b7b4c
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-06-06 21:34:35 +0900

    Title: 일렬로 서있는 소 2, Time: 53ms, Memory: 17MB, Status: Passed - Codetree

--- 250606/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 250606/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -5,6 +5,6 @@ cnt = 0
 for i in range(N):
     for j in range(i+1, N):
         for k in range(j+1, N):
-            if A[i] < A[j] and A[j] < A[k]:
+            if A[i] <= A[j] and A[j] <= A[k]:
                 cnt += 1
 print(cnt)
