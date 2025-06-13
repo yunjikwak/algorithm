97349df95308645b71193ca7d6a6196ff13e64e0
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-06-13 22:26:16 +0900

    Title: 최고의 13위치 2, Time: 49ms, Memory: 17MB, Status: Passed - Codetree

--- 250613/최고의 13위치 2/best-place-of-13-2.py
+++ 250613/최고의 13위치 2/best-place-of-13-2.py
@@ -7,17 +7,21 @@ maps = []
 for _ in range(N):
     maps.append(list(map(int, input().split())))
 
-first = -1
-second = -1
+block = []
 for i in range(N):
     for j in range(N-2):
         cur = maps[i][j] + maps[i][j+1] + maps[i][j+2]
-        if first == -1:
-            first = cur
-        elif second == -1:
-            second = cur
-        elif cur > first:
-            first = cur
-        elif cur > second:
-            second = cur
-print(first + second)
+        block.append((cur, i, j))
+
+result = 0
+for i in range(len(block)):
+    for j in range(i+1, len(block)):
+        val1, x1, y1 = block[i]
+        val2, x2, y2 = block[j]
+
+        if x1 == x2 and not (y1+2 < y2 or y2+2 < y1):
+            continue
+        
+        if result < val1+val2:
+            result = val1 + val2
+print(result)
