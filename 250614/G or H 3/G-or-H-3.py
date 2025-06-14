cb6e757753664ccd399aec0c9062925fa595e8f6
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-06-14 23:01:28 +0900

    Title: G or H 3, Time: 200ms, Memory: 17MB, Status: Passed - Codetree

--- 250614/G or H 3/G-or-H-3.py
+++ 250614/G or H 3/G-or-H-3.py
@@ -2,16 +2,13 @@ import sys
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
-arr = [0] * 10000
-end = 0
+arr = [0] * 10001
 for _ in range(N):
     a, b = input().split()
     arr[int(a)] = b
-    if int(a) > end:
-        end = int(a)
 
 max_score = 0
-for i in range(1, end+1):
+for i in range(1, 10000 - K):
     score = 0
     for j in range(i, i+K+1):
         if arr[j] == 'G':
