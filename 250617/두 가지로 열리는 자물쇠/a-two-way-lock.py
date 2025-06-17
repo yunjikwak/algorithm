4793f8cff5d991eabb766e61b135586206f0a9ab
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-06-17 22:38:46 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 84ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250617/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr1 = list(map(int, input().split()))
+arr2 = list(map(int, input().split()))
+
+canA = []
+canB = []
+for i in arr1:
+    lst = []
+    for k in range(-2, 2+1):
+        lst.append((i + k - 1) % N + 1)
+    canA.append(lst)
+for j in arr2:
+    lst = []
+    for k in range(-2, 2+1):
+        lst.append((j + k - 1) % N + 1)
+    canB.append(lst)
+
+result += len(canA[0]) * len(canA[1]) * len(canA[2])
+result += len(canB[0]) * len(canB[1]) * len(canB[2])
+
+minus = []
+for i in range(3):
+    tmp = 0
+    for j in range(len(canB[i])):
+        if canB[i][j] in canA[i]:
+            tmp += 1
+    minus.append(tmp)
+
+result -= len(minus[0]) * len(minus[1]) * len(minus[2])
+print(result)
