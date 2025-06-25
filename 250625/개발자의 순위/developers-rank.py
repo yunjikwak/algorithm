27734ed53f68fc1b92c80f063b623a555b984320
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-06-25 23:48:53 +0900

    Title: 개발자의 순위, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 250625/개발자의 순위/developers-rank.py
+++ 250625/개발자의 순위/developers-rank.py
@@ -1,14 +1,22 @@
 K, N = map(int, input().split())
 
-arr = [] * (N+1)
-for _ in range(N):
+arr = [[] for _ in range(N+1)]
+for _ in range(K):
     lst = list(map(int, input().split()))
     for i in range(N):
         arr[lst[i]].append(i+1)
 
-print(arr)
+result = 0
+for i in range(1, N+1):
+    for j in range(1, N+1):
+        if i == j:
+            continue
+        upper = True
+        for k in range(K):
+            if arr[i][k] < arr[j][k]:
+                upper = False
+                break
+        if upper:
+            result += 1
 
-# for i in range(N):
-#     for j in range(N):
-#         if 
-            
+print(result)
