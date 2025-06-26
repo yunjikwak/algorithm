6bf938ae4e6829f332c6aac985478306ae9a39f7
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-06-26 21:54:24 +0900

    Title: 상해버린 치즈, Time: 52ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250626/상해버린 치즈/rotten-cheese.py
+++ 250626/상해버린 치즈/rotten-cheese.py
@@ -8,30 +8,27 @@ for _ in range(D):
     p, m, t = map(int, input().split())
     ate[p].append((m, t))
 
-for i in range(len(ate)):
-    ate[i] = sorted(ate[i], key=lambda x: x[1])
-
 sick = [list(map(int, input().split())) for _ in range(S)]
 store = [0] * (M+1)
 
-for i in range(len(sick)):
-    num = sick[i][0]
-    timing = sick[i][1]
-    
+for num, timing in sick:    
     for chz, t in ate[num]:
         if t < timing:
             store[chz] += 1
 
+suspect_cheeses = []
+for i in range(1, M + 1):
+    if store[i] == S:
+        suspect_cheeses.append(i)
+
 result = 0
 
-for chz in range(1, M+1):
-    if store[chz] == len(sick):
-        cnt = 0
-        for i in range(1, N+1):
-            for cheese, _ in ate[i]:
-                if cheese == chz:
-                    cnt += 1
-                    break
-        result = max(result, cnt)
-    
+for chz in suspect_cheeses:
+    cnt = 0
+    for p in range(1, N+1):
+        for n, t in ate[p]:
+            if n == chz:
+                cnt += 1
+                break
+    result = max(result, cnt)
 print(result)
