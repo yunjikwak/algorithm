a548bee833b1b639c89e3076dbfcb2f456d3a048
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-06-27 22:40:59 +0900

    Title: 이상한 폭탄 2, Time: 51ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250627/이상한 폭탄 2/strange-bomb-2.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+bomb = list(int(input().strip()) for _ in range(N))
+
+result = -1
+for i in range(N-K):
+    num = bomb[i]
+    check = False
+    for j in range(1, K+1):
+        if num == bomb[i+j]:
+            check = True
+            break
+    
+    if check:
+        result = max(result, num)
+
+print(result)
