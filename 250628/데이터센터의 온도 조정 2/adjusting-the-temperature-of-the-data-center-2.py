6095271e63a717d9ffd5ca062bbaacc085f0cdb9
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-06-28 22:35:06 +0900

    Title: 데이터센터의 온도 조정 2, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250628/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+N, C, G, H = map(int, input().split())
+arr = []
+
+start = sys.maxsize
+end = -sys.maxsize
+
+for _ in range(N):
+    a,b = map(int, input().split())
+    start = min(start, a)
+    end = max(end, b)
+    arr.append((a,b))
+
+def get_work(temp, electro):
+    T_a, T_b = arr[electro]
+    if temp < T_a:
+        return C
+    elif temp >= T_a and temp <= T_b:
+        return G
+    elif temp > T_b:
+        return H
+
+result = 0
+for i in range(start, end+1):
+    sum_work = 0
+    for n in range(N):
+        work = get_work(i, n)
+        sum_work += work
+    result = max(result, sum_work)
+print(result)
