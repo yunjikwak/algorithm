81bd93038d7207803836a0b087e3eb7ec6fea861
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-06-29 19:02:17 +0900

    Title: 정보에 따른 수 2, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250629/정보에 따른 수 2/number-based-on-information-2.py
+++ 250629/정보에 따른 수 2/number-based-on-information-2.py
@@ -2,36 +2,36 @@ import sys
 input = sys.stdin.readline
 
 T, a, b = map(int, input().split())
-length = b-a+2
-arr = [0] * length
+length = b-a+1
+arr = [0] * 1002
 
 def search(x):
-    s, n = 0, 0
-    for i in range(1, length):
+    s = None
+    n = None
+    for i in range(1001):
         add_pos = x+i
         minus_pos = x-i
 
-        if add_pos < length:
-            if s == 0 and arr[add_pos] == 'S':
+        if add_pos <= b:
+            if s is None and arr[add_pos] == 'S':
                 s = i
-            elif n == 0 and arr[add_pos] == 'N':
+            elif n is None and arr[add_pos] == 'N':
                 n = i
-        if minus_pos >= 0:
-            if s == 0 and arr[minus_pos] == 'S':
+        if minus_pos >= a:
+            if s is None and arr[minus_pos] == 'S':
                 s = i
-            elif n == 0 and arr[minus_pos] == 'N':
+            elif n is None and arr[minus_pos] == 'N':
                 n = i
-        if s != 0 and n != 0:
+        if s is not None and n is not None:
             return s, n
     return 1000, 1000
 
-
 for _ in range(T):
-    a, b = input().split()
-    arr[int(b)] = a
+    t, pos = input().split()
+    arr[int(pos)] = t
 
 result= 0
-for i in range(len(arr)):
+for i in range(a, b+1):
     d1, d2 = search(i)
     if d1 <= d2:
         result += 1
