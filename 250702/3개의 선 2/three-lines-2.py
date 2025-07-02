234c795e03eb471df4747d9618e232e05e960d1b
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-07-02 22:30:36 +0900

    Title: 3개의 선 2, Time: 60ms, Memory: 17MB, Status: Passed - Codetree

--- 250702/3개의 선 2/three-lines-2.py
+++ 250702/3개의 선 2/three-lines-2.py
@@ -2,27 +2,24 @@ import sys
 from collections import Counter
 input = sys.stdin.readline
 
-
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
 
-x_arr = [x[0] for x in arr]
-x_cnt = Counter(x_arr)
+for i in range(3):
+    if not arr:
+        break
+    x_cnt = Counter(a[0] for a in arr)
+    y_cnt = Counter(a[1] for a in arr)
 
-result = 0
+    max_x, max_x_cnt = x_cnt.most_common(1)[0]
+    max_y, max_y_cnt = y_cnt.most_common(1)[0]
 
-for cnt in x_cnt.values():
-    if cnt > 1:
-        result += 1
+    if max_x_cnt >= max_y_cnt:
+        arr = [a for a in arr if a[0] != max_x]
+    else:
+        arr = [a for a in arr if a[1] != max_y]
 
-y_arr = [y[1] for y in arr]
-y_cnt = Counter(y_arr)
-
-for cnt in y_cnt.values():
-    if cnt > 1:
-        result += 1
-
-if result > 3:
-    print(0)
-else:
+if not arr:
     print(1)
+else:
+    print(0)
