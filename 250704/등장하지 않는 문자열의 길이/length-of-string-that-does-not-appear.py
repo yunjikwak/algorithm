da73405554f9e12852089cbb512b51cc6bce858e
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-07-04 23:37:45 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 58ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 250704/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
+++ 250704/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -9,18 +9,20 @@ for i in range(N): # 연속 개수
     isSame = False
     L = i+1
     for j in range(len(arr)-L+1): # 시작 위치
-        word = arr[j:L]
-        for k in range(j + 1, len(arr)-L+1):
-            check = arr[k:k+L]
-            
-            if check == word: # 두 번 이상이면
-                isSame = True
-                break
+        word = arr[j:j+L]
+        for k in range(len(arr)):
+            if k == j or (k < j and k+i > j) or (k > j and k < j+i): # j 범위 포함 X
+                continue
+            else:
+                check = arr[k:k+i+1]
+                if check == word: # 두 번 이상이면
+                    isSame = True
+                    break
         if isSame: # 이미 이 길이의 문자열에서 두 번 이상이면 더 이상 탐색 필요 X
             break
     
     if not isSame: # 전체 다 탐색해도 두 번 이상 X
-        result = L
+        result = i+1
         break
 
 print(result)
