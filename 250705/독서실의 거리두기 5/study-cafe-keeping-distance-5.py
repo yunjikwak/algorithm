a890fce6e7b8479c3117ddd7f569fba5084cd547
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-07-05 21:53:06 +0900

    Title: 독서실의 거리두기 5, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250705/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = list(map(int, input().strip()))
+
+def distance(lst):
+    dis = N
+
+    last = -1
+    for i in range(len(lst)):
+        if lst[i] == 1:
+            if last != -1:
+                dis = min(dis, i-last)
+            last = i
+    return dis
+
+result = 0
+for i in range(N):
+    temp_arr = arr[:i] + [1] + arr[i:]
+    result = max(result, distance(temp_arr))
+print(result)
