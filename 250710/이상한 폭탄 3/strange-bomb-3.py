a412adbbdd9b6425901cc9f1141eb1390802ef93
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-07-10 22:50:43 +0900

    Title: 이상한 폭탄 3, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250710/이상한 폭탄 3/strange-bomb-3.py
+++ 250710/이상한 폭탄 3/strange-bomb-3.py
@@ -7,14 +7,16 @@ bomb = list(int(input()) for _ in range(N))
 max_num = 0
 max_cnt = 0
 for i in range(N-K):
-    cnt = 1
+    cnt = 0
     num = bomb[i]
     for j in range(1, K+1):
         if num == bomb[i+j]:
             cnt += 1
+    if cnt != 0: # 한 번이라도 터지면 자기자신 추가
+        cnt += 1
     if cnt > max_cnt:
         max_cnt = cnt
         max_num = num
-    elif cnt == max_cnt:
+    elif cnt != 0 and cnt == max_cnt:
         max_num = max(max_num, num)
 print(max_num)
