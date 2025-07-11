3c1c8fb9d37810b4ced692954eb4e32633f1c3e1
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-07-11 20:11:40 +0900

    Title: 구간 잘 나누기, Time: 56ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250711/구간 잘 나누기/divide-sections-well.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int,input().split())
+arr = list(map(int, input().split()))
+
+start = max(arr)
+end = sum(arr)
+
+for standard in range(start, end+1):
+    cnt = 0
+    internal_sum = 0
+    for a in arr:
+        internal_sum += a
+        if internal_sum <= standard:
+            continue
+        internal_sum = a
+        cnt += 1
+    if cnt ==  M-1:
+        print(standard)
+        break
