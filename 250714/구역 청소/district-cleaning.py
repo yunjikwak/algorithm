825d12f267e9c373458ea3c8d73af24364886c6b
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-07-14 12:53:11 +0900

    Title: 구역 청소, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250714/구역 청소/district-cleaning.py
+++ 250714/구역 청소/district-cleaning.py
@@ -4,15 +4,11 @@ input = sys.stdin.readline
 a,b = map(int, input().split())
 c,d = map(int, input().split())
 
-result = 0
-if a < c and b > d:
-    result = abs(a-b)
-elif c < a and d > b:
-    result = abs(c-d)
-else:
-    result = abs(a-b) + abs(c-d)
-    if a < c and b > c:
-        result -= b-c
-    elif a > c and d > a:
-        result -= d-a
+result = abs(a-b) + abs(c-d)
+
+start = max(a,c)
+end = min(b, d)
+
+result -= end-start
+
 print(result)
