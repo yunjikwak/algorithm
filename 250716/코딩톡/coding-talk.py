2445b0ca79902a3c6bfae61083fb7319a56a1e9c
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-07-16 15:11:55 +0900

    Title: 코딩톡, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250716/코딩톡/coding-talk.py
+++ 250716/코딩톡/coding-talk.py
@@ -2,7 +2,7 @@ import sys
 input = sys.stdin.readline
 
 N, M, p = map(int, input().split())
-msg_record = list(tuple(input().split()) for _ in range(N))
+msg_record = list(tuple(input().split()) for _ in range(M))
 
 unread = [1] * N
 
@@ -19,7 +19,7 @@ while True:
     else:
         break
 
-for i in range(p-1, N):
+for i in range(p-1, M):
     person, status = msg_record[i]
     idx = ord(person) - ord('A')  # 'A' → 0, 'B' → 1, ...
     unread[idx] = 0 # read
