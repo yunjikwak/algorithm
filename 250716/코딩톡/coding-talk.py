2cad94f60543d53b31c688a6a6f909c2cd3b81ce
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-07-16 15:06:41 +0900

    Title: 코딩톡, Time: 80ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250716/코딩톡/coding-talk.py
+++ 250716/코딩톡/coding-talk.py
@@ -10,6 +10,15 @@ _, status = msg_record[p-1]
 if int(status) == 0:
     exit()
 
+check = p-2
+while True:
+    a, b = msg_record[check]
+    if int(b) == int(status):
+        unread[ord(a) - ord('A')] = 0
+        check -= 1
+    else:
+        break
+
 for i in range(p-1, N):
     person, status = msg_record[i]
     idx = ord(person) - ord('A')  # 'A' → 0, 'B' → 1, ...
