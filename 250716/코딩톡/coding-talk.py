a67ac03c24ecded563b29d79ebcea56e8c05a158
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-07-16 14:52:01 +0900

    Title: 코딩톡, Time: 52ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250716/코딩톡/coding-talk.py
+++ 250716/코딩톡/coding-talk.py
@@ -4,13 +4,17 @@ input = sys.stdin.readline
 N, M, p = map(int, input().split())
 msg_record = list(tuple(input().split()) for _ in range(N))
 
-unread = [1] * M
+unread = [1] * N
+
+_, status = msg_record[p-1]
+if int(status) == 0:
+    exit()
 
 for i in range(p-1, N):
     person, status = msg_record[i]
     idx = ord(person) - ord('A')  # 'A' → 0, 'B' → 1, ...
     unread[idx] = 0 # read
 
-for idx in range(M):
+for idx in range(N):
     if unread[idx]:
         print(chr(ord('A') + idx), end=' ') # how to change 0 -> A ...
