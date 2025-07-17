436db1762a341f0e61c64d1c7620ecbd90c025e7
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-07-17 18:13:04 +0900

    Title: X 달리기, Time: 249ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 250717/X 달리기/x-run.py
+++ 250717/X 달리기/x-run.py
@@ -2,23 +2,23 @@ import sys
 input = sys.stdin.readline
 
 X = int(input())
-least_time = sys.maxsize
+min_time = sys.maxsize
 
-for i in range(X):
+for i in range(1, X+1):
     total = 0
     time_taken = 0
-    speed = 0
-    while speed <= i:
-        speed += 1
-        total += speed
+
+    speed = 1
+
+    for up in range(1, i):
+        total += up
         time_taken += 1
-    while speed > 1:
-        speed -= 1
-        total += speed
+    total += i
+    time_taken += 1
+    for down in range(i-1, 0, -1):
+        total += down
         time_taken += 1
-    if total > X:
-        break
     if total < X:
         time_taken += X-total
-    least_time = min(time_taken, least_time)
-print(least_time)
+    min_time = min(time_taken, min_time)
+print(min_time)
