b737e180de9e388e38749718d6fc16f49ab51c75
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-07-17 18:22:29 +0900

    Title: X 달리기, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 250717/X 달리기/x-run.py
+++ 250717/X 달리기/x-run.py
@@ -5,20 +5,13 @@ X = int(input())
 min_time = sys.maxsize
 
 for i in range(1, X+1):
-    total = 0
-    time_taken = 0
-
-    speed = 1
-
-    for up in range(1, i):
-        total += up
-        time_taken += 1
-    total += i
-    time_taken += 1
-    for down in range(i-1, 0, -1):
-        total += down
-        time_taken += 1
+    time_taken = (2 * i) - 1
+    total = i * i
+    
     if total < X:
-        time_taken += X-total
+        remain = X - total
+        additional_time = (remain + i - 1) // i
+        time_taken += additional_time
+
     min_time = min(time_taken, min_time)
 print(min_time)
