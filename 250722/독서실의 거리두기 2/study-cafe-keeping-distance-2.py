c479806cf4406dae1d6f368695ab80509e291a64
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-07-22 20:25:20 +0900

    Title: 독서실의 거리두기 2, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250722/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
+++ 250722/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -5,14 +5,16 @@ N = int(input())
 arr = list(map(int, input().strip()))
 pos = [i for i, v in enumerate(arr) if v == 1]
 
+closer = len(arr)
 min_max = 0
 for p in range(1, len(pos)):
     diff = pos[p] - pos[p-1]
-    if min_max*2 < diff:
-        tmp = diff // 2
-        min_max = tmp
+    if closer > diff:
+        closer = diff
+    min_max = max(min_max, diff // 2)
 
 if arr[N-1] == 0:
     min_max = max(min_max, N-1-pos[-1])
+min_max = min(min_max, closer)
 
 print(min_max)
