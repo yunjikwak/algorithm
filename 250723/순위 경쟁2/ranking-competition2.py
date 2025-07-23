add8a7b8100b2b056f7f1754f01283aff3a7898e
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-07-23 19:16:47 +0900

    Title: 순위 경쟁2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250723/순위 경쟁2/ranking-competition2.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+score = list(tuple(input().split()) for _ in range(N))
+
+a, b = 0, 0
+first = None
+cnt = 0
+for c, s in score:
+    s = int(s)
+    if c == 'A':
+        a += s
+    elif c == 'B':
+        b += s
+
+    if a > b:
+        cur_first = 'A'
+    elif a < b:
+        cur_first = 'B'
+    elif a == b:
+        cur_first = 'AB'
+
+    if first is None:
+        first = cur_first
+        cnt += 1
+    elif first != cur_first:
+        first = cur_first
+        cnt += 1
+print(cnt)
