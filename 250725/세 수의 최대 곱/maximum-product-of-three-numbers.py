d8c94936765848b9b0bb838fe5f3bbd9b2f87663
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-07-25 14:59:44 +0900

    Title: 세 수의 최대 곱, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250725/세 수의 최대 곱/maximum-product-of-three-numbers.py
+++ 250725/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -8,10 +8,11 @@ arr = list(map(int, input().split()))
 # 모두 음 -> 절대값 낮은 것
 
 arr.sort()
-result = 0
+
+result = -sys.maxsize
 if arr[0] < 0 and arr[-1] < 0: # 전체 음
     result = arr[0] * arr[1] * arr[2]
 else:
-    result = max(result, arr[-1]*arr[-2]*arr[-3]) # 양3
+    result = max(result, arr[-1]*arr[-2]*arr[-3])
     result = max(result, arr[-1]*arr[0]*arr[1])
 print(result)
