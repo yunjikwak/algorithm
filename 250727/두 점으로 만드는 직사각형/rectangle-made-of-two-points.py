c230f2efc92eca90357cf3c54a519d24d97d0e57
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-07-27 18:18:20 +0900

    Title: 두 점으로 만드는 직사각형, Time: 40ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250727/두 점으로 만드는 직사각형/rectangle-made-of-two-points.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+x1, y1, x2, y2 = map(int, input().split())
+a1, b1, a2, b2 = map(int, input().split())
+
+A = min(a1, x1)
+B = min(b1, y1)
+C = max(a2, x2)
+D = max(b2, y2)
+
+print((C-A)*(D-B))
