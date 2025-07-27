fc7b53b2c35cb4afb250a11a2ae88cfcc55f40bb
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-07-27 18:54:38 +0900

    Title: 전부 포함하는 선분, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250727/전부 포함하는 선분/all-inclusive-segment.py
+++ 250727/전부 포함하는 선분/all-inclusive-segment.py
@@ -10,4 +10,7 @@ a2 = sorted(arr, key=lambda x:x[0])
 result = a1[-2][1] - a2[0][0]
 result = min(result, a1[-1][1] - a2[1][0])
 
+if a1[-1] == a2[0]:
+    result = min(result, a1[-2][1] - a2[1][0])
+
 print(result)
