8aef4e9269267843984483466693e45cb634b81d
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-07-30 22:35:43 +0900

    Title: 행복한 수열의 개수, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250730/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(N)]
+
+result = 0
+for i in range(N):
+    cur = board[i][0]
+    cnt = 1
+    for j in range(1, N):
+        if cur == board[i][j]:
+            cnt += 1
+        else:
+            cur = board[i][j]
+            cnt_row = 1
+        
+        if cnt >= M:
+            result += 1
+            break
+
+    cur = board[0][i]
+    cnt = 1
+    for j in range(1, N):
+        if cur == board[j][i]:
+            cnt += 1
+        else:
+            cur = board[j][i]
+            cnt = 1
+        
+        if cnt >= M:
+            result += 1
+            break
+
+print(result)
