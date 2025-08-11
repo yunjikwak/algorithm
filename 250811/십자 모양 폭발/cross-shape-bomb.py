0ba8eca77934d7eddfb77d6e1bbb99d2a6a3f2ff
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-08-11 23:49:11 +0900

    Title: 십자 모양 폭발, Time: 74ms, Memory: 19MB, Status: Passed - Codetree

--- 250811/십자 모양 폭발/cross-shape-bomb.py
+++ 250811/십자 모양 폭발/cross-shape-bomb.py
@@ -4,41 +4,64 @@ input = sys.stdin.readline
 N = int(input())
 graph = [list(map(int, input().split())) for _ in range(N)]
 r,c = map(int, input().split())
+r, c = r - 1, c - 1
 
-size = graph[r-1][c-1]
-half = size-1
+if graph[r][c] > 0:
+    bomb_size = graph[r][c]
+    graph[r][c] = 0
 
-col = []
-for i in range(N):
-    col.append(graph[i][c-1])
+    for i in range(1, bomb_size):
+        if r - i >= 0: graph[r - i][c] = 0
+        if r + i < N: graph[r + i][c] = 0
+        if c - i >= 0: graph[r][c - i] = 0
+        if c + i < N: graph[r][c + i] = 0
+    
+temp_graph = [[0] * N for _ in range(N)]
 
-if (size*2-1) < N:
-    if (r-1-half) < 0:
-        times = r + half
-        col = [0 for _ in range(times)] + col[times:]
-    elif (r-1+half) >= N:
-        col = col[:r-1-half]
-        col = [0 for _ in range(N-len(col))] + col
-    else:
-        col = col[:r-1-half] + col[r+half:]
-        col = [0 for _ in range(N-len(col))] + col
-else:
-    col = [0 for _ in range(N)]
-
-# print(col)
-
-for i in range(N-1, -1, -1):
-    for j in range(N):
-        if j == c-1: # 터진 열
-            graph[i][j] = col[i]
-            continue
-        elif i > r-1 or j < (c-1-half) or j > c-1+half: # 범위 밖
-            continue
-        
-        if i == 0:
-            graph[i][j] = 0
-        else:
-            graph[i][j] = graph[i-1][j]
-# print(graph)
+for j in range(N):
+    temp_row_idx = N - 1
+    for i in range(N - 1, -1, -1):
+        if graph[i][j] != 0:
+            temp_graph[temp_row_idx][j] = graph[i][j]
+            temp_row_idx -= 1
+graph = temp_graph
 for r in graph:
     print(*r)
+
+# size = graph[r-1][c-1]
+# half = size-1
+
+# col = []
+# for i in range(N):
+#     col.append(graph[i][c-1])
+
+# if (size*2-1) < N:
+#     if (r-1-half) < 0:
+#         times = r + half
+#         col = [0 for _ in range(times)] + col[times:]
+#     elif (r-1+half) >= N:
+#         col = col[:r-1-half]
+#         col = [0 for _ in range(N-len(col))] + col
+#     else:
+#         col = col[:r-1-half] + col[r+half:]
+#         col = [0 for _ in range(N-len(col))] + col
+# else:
+#     col = [0 for _ in range(N)]
+
+# # print(col)
+
+# for i in range(N-1, -1, -1):
+#     for j in range(N):
+#         if j == c-1: # 터진 열
+#             graph[i][j] = col[i]
+#             continue
+#         elif i > r-1 or j < (c-1-half) or j > c-1+half: # 범위 밖
+#             continue
+        
+#         if i == 0:
+#             graph[i][j] = 0
+#         else:
+#             graph[i][j] = graph[i-1][j]
+# # print(graph)
+# for r in graph:
+#     print(*r)
