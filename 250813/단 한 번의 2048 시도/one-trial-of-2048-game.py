58dc2309bac4f7524f258e1fdce4cc1a3d9e242c
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-08-13 23:35:35 +0900

    Title: 단 한 번의 2048 시도, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250813/단 한 번의 2048 시도/one-trial-of-2048-game.py
+++ 250813/단 한 번의 2048 시도/one-trial-of-2048-game.py
@@ -1,7 +1,64 @@
 import sys
 input = sys.stdin.readline
 
-arr = [list(map(int, input.split())) for _ in range(4)]
-d = input()
+arr = [list(map(int, input().split())) for _ in range(4)]
+d = input().strip()
 
-print(arr)
+def short(a):
+    tmp = []
+    for i in range(len(a)):
+        if a[i] != 0:
+            tmp.append(a[i])
+    return tmp
+
+def calc_arr(a, reverse=False):
+    a = short(a)
+    if reverse:
+        a = a[::-1]
+
+    store = []
+    jump = False
+    for i in range(len(a)):
+        if jump:
+            jump = False
+            continue
+        if i < len(a) - 1 and a[i] == a[i+1]:
+            store.append(a[i] + a[i])
+            jump = True
+        else:
+            store.append(a[i])
+
+    while len(store) < 4:
+        store.append(0)
+
+    if reverse:
+        store = store[::-1]
+    return store
+
+if d == 'L':
+    for i in range(4):
+        arr[i] = calc_arr(arr[i], reverse=False)
+elif d == 'R':
+    for i in range(4):
+        arr[i] = calc_arr(arr[i], reverse=True)
+elif d == 'U':
+    for i in range(4):
+        store = []
+        for j in range(4):
+            store.append(arr[j][i])
+        col = calc_arr(store, reverse=False)
+
+        for j in range(4):
+            arr[j][i] = col[j]
+elif d == 'D':
+    for i in range(4):
+        store = []
+        for j in range(4):
+            store.append(arr[j][i])
+        col = calc_arr(store, reverse=True)
+
+        for j in range(4):
+            arr[j][i] = col[j]
+
+for row in arr:
+    print(*row)
