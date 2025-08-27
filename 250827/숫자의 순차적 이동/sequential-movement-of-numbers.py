ccfd54303422f5d618e1cf845e89cc4791c5ce39
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-08-27 21:11:54 +0900

    Title: 숫자의 순차적 이동, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250827/숫자의 순차적 이동/sequential-movement-of-numbers.py
+++ 250827/숫자의 순차적 이동/sequential-movement-of-numbers.py
@@ -6,10 +6,33 @@ dx = [-1, -1, -1, 0, 1, 1, 1, 0]
 dy = [-1, 0, 1, 1, 1, 0, -1, -1]
 
 arr = [list(map(int, input().split())) for _ in range(N)]
+
+def inRange(a, b):
+    return 0 <= a < N and 0 <= b < N
+
 for _ in range(M):
-    seq = [] * (N*N+1)
-    print(seq[a] = [(1, 2)])
-    # for i in range(N):
-    #     for j in range(N):
-    #         seq[arr[i][j]] = [(i, j)]
+    seq = [None] * (N*N)
+
+    for i in range(N):
+        for j in range(N):
+            seq[arr[i][j]-1] = (i, j)
     print(seq)
+
+    for x, y in seq:
+        max_num = 0
+        a, b = -1, -1
+
+        for i in range(8):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if inRange(nx, ny) and max_num < arr[nx][ny]:
+                max_num = arr[nx][ny]
+                a = nx
+                b = ny
+        print(arr[x][y], arr[a][b])
+        tmp = arr[x][y]
+        arr[x][y] = max_num
+        arr[a][b] = tmp
+
+for row in arr:
+    print(*row)
