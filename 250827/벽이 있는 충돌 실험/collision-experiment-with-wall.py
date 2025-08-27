d36e78fdd115417f6f81e108460fae058096384b
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-08-27 22:32:39 +0900

    Title: 벽이 있는 충돌 실험, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250827/벽이 있는 충돌 실험/collision-experiment-with-wall.py
+++ 250827/벽이 있는 충돌 실험/collision-experiment-with-wall.py
@@ -10,14 +10,6 @@ dy = [0, 0, -1, 1]
 def inRange(a,b):
     return 0<=a<N and 0<=b<N
 
-def search_marble(arr):
-    marble = []
-    for i in range(N):
-        for j  in range(N):
-            if arr[i][j][0] == 1:
-                marble.append((i, j, arr[i][j][1]))
-    return marble
-
 def opposite(d):
     if d == 0: return 1
     elif d == 1: return 0
@@ -42,25 +34,35 @@ for _ in range(T):
         if not marble:
             break
 
-        n_cnt = [[[0,0] for _ in range(N)] for _ in range(N)]
+        # n_cnt = [[[0,0] for _ in range(N)] for _ in range(N)]
+        n_cnt = {}
 
         for x, y, d in marble:
             nx = x+dx[d]
             ny = y+dy[d]
 
             if inRange(nx, ny):
-                n_cnt[nx][ny][0] += 1
-                n_cnt[nx][ny][1] = d
+                if (nx,ny) not in n_cnt:
+                    n_cnt[(nx, ny)] = []
+                n_cnt[(nx, ny)].append(d)
+                # n_cnt[nx][ny][0] += 1
+                # n_cnt[nx][ny][1] = d
             else:
-                n_cnt[x][y][0] += 1
-                n_cnt[x][y][1] = opposite(d)
+                if (nx,ny) not in n_cnt:
+                    n_cnt[(nx, ny)] = []
+                n_cnt[(nx, ny)].append(opposite(d))
+                # n_cnt[x][y][0] += 1
+                # n_cnt[x][y][1] = opposite(d)
 
         n_marble = []
-        for r in range(N):
-            for c in range(N):
-                if n_cnt[r][c][0] == 1:
-                    nd = n_cnt[r][c][1]
-                    n_marble.append([r, c, nd])
+        for (x,y), dirs in n_cnt.items():
+            if len(dirs) == 1:
+                n_marble.append((x,y,dirs[0]))
+        # for r in range(N):
+        #     for c in range(N):
+        #         if n_cnt[r][c][0] == 1:
+        #             nd = n_cnt[r][c][1]
+        #             n_marble.append([r, c, nd])
 
         marble = n_marble
         
