09ed0dbea8b2dc7f2c644d4b825dc753e42c94e3
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-08-27 20:58:06 +0900

    Title: 숫자의 순차적 이동, Time: 72ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250827/숫자의 순차적 이동/sequential-movement-of-numbers.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+dx = [-1, -1, -1, 0, 1, 1, 1, 0]
+dy = [-1, 0, 1, 1, 1, 0, -1, -1]
+
+arr = [list(map(int, input().split())) for _ in range(N)]
+for _ in range(M):
+    seq = [] * (N*N+1)
+    print(seq[a] = [(1, 2)])
+    # for i in range(N):
+    #     for j in range(N):
+    #         seq[arr[i][j]] = [(i, j)]
+    print(seq)
