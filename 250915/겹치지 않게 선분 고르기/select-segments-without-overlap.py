e329a5832168a25d42ae308302d96746d384bcd8
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-09-15 21:04:02 +0900

    Title: 겹치지 않게 선분 고르기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250915/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -0,0 +1,43 @@
+N = int(input())
+
+line = []
+for _ in range(N):
+    line.append((list(map(int, input().split()))))
+
+result = 0
+store = []
+
+def recur(num):
+    global result
+
+    if num == N:
+        # print(store)
+        result = max(result, len(store))
+        return
+    elif len(store) >= 2:
+        nl, nr = line[store[-1]]
+        ok = True
+        for i in range(len(store)-1):
+            a, b = line[store[i]]
+
+            # 겹치지 않는 경우
+                # nl이 b보다 크거나
+                # nr이 a보다 작음
+            if nl > b or nr < a:
+                continue
+            else:
+                ok = False
+                break
+        if not ok:
+            return
+    
+    for select in range(num, len(line)):
+        # print("select", select, num)
+        store.append(select)
+        recur(select+1)
+        store.pop()
+
+    return
+
+recur(0)
+print(result)
