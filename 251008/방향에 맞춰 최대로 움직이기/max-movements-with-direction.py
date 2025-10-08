d479a29c716c2665db85a6bf796e4e72305ea5a1
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-10-08 23:29:13 +0900

    Title: 방향에 맞춰 최대로 움직이기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251008/방향에 맞춰 최대로 움직이기/max-movements-with-direction.py
@@ -0,0 +1,10 @@
+N = int(input())
+arr = []*N
+# for i in range(N):
+#     row = list(map(int, input().split()))
+#     for j in row:
+#         arr[i].append([j, -1])
+
+# r,c = map(int, input().split())
+
+print(arr)
