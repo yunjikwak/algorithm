9c8c69e1aabdcc2223a05729d0ef8881a5ad67d0
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-10-11 21:33:08 +0900

    Title: XOR 결과 최대 만들기, Time: 74ms, Memory: 19MB, Status: Passed - Codetree

--- 251011/XOR 결과 최대 만들기/max-of-xor.py
+++ 251011/XOR 결과 최대 만들기/max-of-xor.py
@@ -4,14 +4,13 @@ arr = list(map(int, input().split()))
 result = 0
 def bt(val, idx, cnt):
     global result
-    
+
     if cnt == M:
         result = max(result, val)
         return
     
     for i in range(idx, len(arr)):
         bt(val^arr[i], i+1, cnt+1)
-        bt(val, i+1, cnt)
     return
 
 bt(0, 0, 0)
