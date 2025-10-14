681f4b5bbc61b62cd24120e767787c5bd477a695
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2025-10-14 21:24:07 +0900

    Title: N개의 점 중 M개 고르기, Time: 174ms, Memory: 23MB, Status: Passed - Codetree

--- 251014/N개의 점 중 M개 고르기/choose-m-out-of-n-points.py
+++ 251014/N개의 점 중 M개 고르기/choose-m-out-of-n-points.py
@@ -6,17 +6,22 @@ for _ in range(N):
     arr.append((a,b))
 
 store = []
-dist = 100*100*2
+dist = 100*100*100
 
 def bt(idx):
     global dist
-    
+
     if len(store) == M:
-        store.sort()
-        first, last = store[0], store[-1]
-        cur = (first[0]-last[0])*(first[0]-last[0]) + (first[1]-last[1])*(first[1]-last[1])
-        dist = min(dist, cur)
+        cur_max = 0
+        
+        for p1 in store:
+            for p2 in store:
+                dist_sq = (p1[0] - p2[0])**2 + (p1[1] - p2[1])**2
+                cur_max = max(cur_max, dist_sq)
+            
+        dist = min(dist, cur_max)
         return
+        
     if idx == N-1:
         return
     
