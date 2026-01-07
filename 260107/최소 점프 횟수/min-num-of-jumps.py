37478499351ffee563aa68f32f5d8486791e32a5
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2026-01-07 13:05:19 +0900

    Title: 최소 점프 횟수, Time: 81ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 260107/최소 점프 횟수/min-num-of-jumps.py
@@ -0,0 +1,22 @@
+N = int(input())
+arr = list(map(int, input().split()))
+
+cnt = N
+
+def bt(idx, cur_cnt):
+    global cnt
+    
+    if idx == N-1:
+        cnt = min(cnt, cur_cnt)
+        return
+    
+    for i in range(1, arr[idx]+1):
+        bt(idx+i, cur_cnt+1)
+    
+    return
+
+bt(0, 0)
+if cnt == N:
+    print(-1)
+else:
+    print(cnt)
