0f8889a4f08ea49b2d76dc774ad6fefeff33eb72
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2026-01-07 13:18:01 +0900

    Title: 최소 점프 횟수, Time: 51ms, Memory: 16MB, Status: Passed - Codetree

--- 260107/최소 점프 횟수/min-num-of-jumps.py
+++ 260107/최소 점프 횟수/min-num-of-jumps.py
@@ -5,14 +5,14 @@ cnt = N
 
 def bt(idx, cur_cnt):
     global cnt
-    
+
     if idx == N-1:
         cnt = min(cnt, cur_cnt)
         return
     
     for i in range(1, arr[idx]+1):
-        bt(idx+i, cur_cnt+1)
-    
+        if idx + i < N:
+            bt(idx+i, cur_cnt+1)
     return
 
 bt(0, 0)
