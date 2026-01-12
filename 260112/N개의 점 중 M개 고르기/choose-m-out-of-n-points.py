4387c901938fa5e5b103c8c227dd90f104d919fb
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2026-01-12 12:38:14 +0900

    Title: N개의 점 중 M개 고르기, Time: 175ms, Memory: 23MB, Status: Passed - Codetree

--- 260112/N개의 점 중 M개 고르기/choose-m-out-of-n-points.py
+++ 260112/N개의 점 중 M개 고르기/choose-m-out-of-n-points.py
@@ -5,36 +5,27 @@ for i in range(N):
     a, b = map(int, input().split())
     pos.append((a, b))
 
-pos.sort()
-
 select = []
-dist = 0
+dist = 100**2*2
 
 def calc(a, b):
-    diff_x = abs(a[0] - b[0])
-    diff_y = abs(a[1] - b[1])
-    return diff_x**2 + diff_y**2
+    return (a[0] - b[0])**2 + (a[1] - b[1])**2
 
-def can():
-    if abs(select[0] - select[1]) >= M-1:
-        return True
-    else:
-        return False
-
-dist = calc(pos[0], pos[-1])
 def bt(idx):
     global dist
     
-    if len(select) == 2:
-        if can():
-            dist = min(dist, calc(pos[select[0]], pos[select[1]]))
-        return
-    
-    if idx == N:
+    if len(select) == M:
+        cur = 0
+        
+        for i in range(M):
+            for j in range(i + 1, M):
+                cur = max(cur, calc(select[i], select[j]))
+        
+        dist = min(dist, cur)
         return
 
     for i in range(idx, N):
-        select.append(i)
+        select.append(pos[i])
         bt(i+1)
         select.pop()
 
