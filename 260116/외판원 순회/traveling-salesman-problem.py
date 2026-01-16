efdc38513ee17cfba176981a22957b0b4be39d6d
yunjikwak <83887485+yunjikwak@users.noreply.github.com>
2026-01-16 12:26:02 +0900

    Title: 외판원 순회, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260116/외판원 순회/traveling-salesman-problem.py
@@ -0,0 +1,25 @@
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+
+visited = [False] * N
+result = 10000*N
+
+def choose(row, cnt, cur):
+    global result
+
+    if cnt == N-1:
+        cur += arr[row][0]    
+        result = min(result, cur)
+        return
+
+    for i in range(1, N):
+        if visited[i]:
+            continue
+        if arr[row][i] == 0:
+            continue
+        visited[i] = True
+        choose(i, cnt+1, cur+arr[row][i])
+        visited[i] = False
+    
+choose(0, 0, 0)
+print(result)
